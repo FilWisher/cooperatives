4b6a8c69e82e57ce0a22b450120ddd5507088fd3
FilWisher <williamfisher@hotmail.co.uk>
2016-02-27 18:07:51 +0000

    adds content management for what

---
+++ static/cms.js
@@ -0,0 +1,35 @@
+function app(section) {
+  'use strict'
+  
+  var renderer = new marked.Renderer()
+  renderer.hr = function (text, level) {
+    return '<div class="line">'
+      + '<img src="http://i.imgur.com/5CNWqYr.png" alt="line" class="line">'
+      + '</div>'
+  }
+
+  function get(url, cb) {
+    try {
+      var x = new XMLHttpRequest()
+      x.open('GET', url, 1)
+      x.onreadystatechange = function () {
+        x.readyState > 3 && cb && cb(x.responseText, x)
+      };
+      x.send()
+    } catch (e) {
+      window.console && console.log(e) 
+    }
+  }
+
+  function getContent() {
+    get('/content/'+section+'.md', function (d) {
+      try {
+        document.querySelector("#" + section).innerHTML = marked(d, {renderer:renderer})
+      } catch (e) {
+        window.console && console.log(e)
+      }
+    })
+  }
+  
+  getContent()
+}
