794cd320796a64a58a14833865d25df55ace624e
FilWisher <williamfisher@hotmail.co.uk>
2016-04-05 11:20:16 +0100

    updates news page layout and adds image support

--- static/cms.js
+++ static/cms.js
@@ -3,7 +3,7 @@ function app(section) {
   
   var renderer = new marked.Renderer()
   renderer.hr = function (text, level) {
-    return ''
+    return '<p class="line">------------------------------------------------------</p>'
   }
   
   renderer.blockquote = function (text) {
